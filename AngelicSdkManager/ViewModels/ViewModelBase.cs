d5bf9ca953c9275183df0d51f43522d6a1124a1c
Aidan Fell <a.fell1@protonmail.com>
2025-08-31 22:53:22 +0100

    initial commit apart from slnx

---
+++ AngelicSdkManager/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AngelicSdkManager.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ AngelicSdkManager/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AngelicSdkManager.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
